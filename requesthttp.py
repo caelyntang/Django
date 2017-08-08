490cef91cbf10206b2394af8f402acaa65fd18c6
South <caelyntang@163.com>
2017-08-08 19:07:41 +0800

    test

---
+++ requesthttp.py
@@ -0,0 +1,15 @@
+#encoding=utf-8
+import requests
+
+# response=requests.get('http://www.baidu.com')
+# print(type(response))
+
+# from flask import Flask
+# app=Flask(__name__)
+# @app.route('/test',methods=['GET'])
+# def testHelloWorldHttp():
+#     return 'hello world http'
+#
+# if __name__=='__main_':
+#     app.run('127.0.0.1', 8888, debug=True)
+#     app.run('127.0.0.1', 8889, debug=True,ssl_context='adhoc')
